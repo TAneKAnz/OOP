6af64346f3190284542ad6f1cdf781320249e63c
TAneKAnz <taninwlol001@gmail.com>
2022-02-11 17:52:54 +0700

    last fix

--- Problem4_1.java
+++ Problem4_1.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class Problem4_1 {
     public static void main(String[] args) {
-        Stock s1 = new Stock(34.5, 34.35);
+        Stock s1 = new Stock("ORCL", "Oracle Coperation");
+        s1.getPrice(34.35, 34.5);
         System.out.println("Symbol: " + s1.symbol);
         System.out.println("Name: " + s1.name);
         System.out.println("Previous Closing Price: " + s1.previousClosingPrice);
@@ -11,12 +12,17 @@ public class Problem4_1 {
 }
 
 class Stock {
-    String symbol = "ORCL";
-    String name = "Oracle Coperation";
+    String symbol;
+    String name;
     double previousClosingPrice;
     double currentPrice;
 
-    Stock(double newPrevious, double newCurrent) {
+    Stock(String newSymbol, String newName) {
+        symbol = newSymbol;
+        name = newName;
+    }
+
+    void getPrice(double newPrevious, double newCurrent) {
         previousClosingPrice = newPrevious;
         currentPrice = newCurrent;
     }
