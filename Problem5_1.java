525cdfb4888effca0bc5e19d0a7f8250250d9768
TAneKAnz <taninwlol001@gmail.com>
2022-02-18 03:47:23 +0700

    finished 5-1

--- Problem5_1.java
+++ Problem5_1.java
@@ -1,26 +1,25 @@
-public class Test {
+import java.util.Date;
+public class Problem5_1 {
     public static void main(String[] args) {
-        Account account = new Account(1102, 20000);
+        Account account = new Account(1122, 20000);
         account.setAnnualInterestRate(4.5);
-        account.withdraw(2500.0);
-        account.deposit(3000.0);
+        account.withdraw(2500);
+        account.deposit(3000);
         System.out.println("Balance: $" + account.getBalance());
         System.out.println("Monthly Interest: " + account.getMonthlyInterest());
         System.out.println("Date Created: " + account.getDateCreated());
-
     }
 }
 
 class Account {
-    private int id = 0;
+    private int id;
     private double balance;
-    private double annualInterestRate = 4.5;
-    private java.util.Date dateCreated;
+    private double annualInterestRate;
+    private Date dateCreated;
 
     public Account() {
-        dateCreated = new java.util.Date();
+        dateCreated = new Date();
     }
-
     public Account(int id, double balance) {
         this();
         this.id = id;
@@ -28,45 +27,36 @@ class Account {
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
-
-    public double getBalance() {
-        return this.balance;
-    }
-
-    public double getAnnualInterestRate() {
-        return annualInterestRate;
-    }
-
-    public String getDateCreated() {
-        return this.dateCreated.toString();
-    }
-
     public void setId(int id) {
         this.id = id;
     }
-
+    public double getBalance() {
+        return balance;
+    }
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
     public void setAnnualInterestRate(double annualInterestRate) {
         this.annualInterestRate = annualInterestRate;
     }
+    public String getDateCreated() {
+        return dateCreated.toString();
+    }
 
     public double getMonthlyInterestRate() {
-        return (annualInterestRate / 100) / 12 ;
+        return (annualInterestRate/100)/12;
     }
-
     public double getMonthlyInterest() {
-        return balance * getMonthlyInterestRate();
+        return balance*getMonthlyInterestRate();
     }
-
     public void withdraw(double amount) {
         this.balance -= amount;
     }
-
     public void deposit(double amount) {
         this.balance += amount;
     }
